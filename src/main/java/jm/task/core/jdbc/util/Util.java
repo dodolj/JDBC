1f681c3ee2181b4cda17c13ce7c877baacc41592
dodolj <nordost777@bk.ru>
2025-03-10 20:32:52 +0300

    innitJdbcApp

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,8 @@
+package jm.task.core.jdbc.util;
+import java.sql.Connection;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    //открыть connection с БД (в java.SQL.
+    //собрать "transactionManager"
+}
